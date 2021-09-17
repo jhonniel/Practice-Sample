610fa85199fd7243938a0437deded00e195e6e29
unknown <jhonnielygay99@gmail.com>
2021-09-17 21:00:01 +0800

    btn click

---
+++ Practice_Sample/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblname = New System.Windows.Forms.Label()
+        Me.lblage = New System.Windows.Forms.Label()
+        Me.lbltalent = New System.Windows.Forms.Label()
+        Me.lblstatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn
+        '
+        Me.btn.Location = New System.Drawing.Point(184, 302)
+        Me.btn.Name = "btn"
+        Me.btn.Size = New System.Drawing.Size(88, 37)
+        Me.btn.TabIndex = 1
+        Me.btn.Text = "Click Me!"
+        Me.btn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label1.Location = New System.Drawing.Point(159, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 46)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Hello!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label2.Location = New System.Drawing.Point(63, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Name: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label3.Location = New System.Drawing.Point(65, 131)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Age: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label4.Location = New System.Drawing.Point(63, 172)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(160, 25)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Talent/Hobblies: "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Label5.Location = New System.Drawing.Point(65, 213)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(172, 25)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Single or Taken?: "
+        '
+        'lblname
+        '
+        Me.lblname.AutoSize = True
+        Me.lblname.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblname.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.lblname.Location = New System.Drawing.Point(144, 92)
+        Me.lblname.Name = "lblname"
+        Me.lblname.Size = New System.Drawing.Size(0, 25)
+        Me.lblname.TabIndex = 7
+        '
+        'lblage
+        '
+        Me.lblage.AutoSize = True
+        Me.lblage.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblage.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.lblage.Location = New System.Drawing.Point(144, 131)
+        Me.lblage.Name = "lblage"
+        Me.lblage.Size = New System.Drawing.Size(0, 25)
+        Me.lblage.TabIndex = 8
+        '
+        'lbltalent
+        '
+        Me.lbltalent.AutoSize = True
+        Me.lbltalent.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltalent.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.lbltalent.Location = New System.Drawing.Point(229, 172)
+        Me.lbltalent.Name = "lbltalent"
+        Me.lbltalent.Size = New System.Drawing.Size(0, 25)
+        Me.lbltalent.TabIndex = 9
+        '
+        'lblstatus
+        '
+        Me.lblstatus.AutoSize = True
+        Me.lblstatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblstatus.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.lblstatus.Location = New System.Drawing.Point(243, 213)
+        Me.lblstatus.Name = "lblstatus"
+        Me.lblstatus.Size = New System.Drawing.Size(0, 25)
+        Me.lblstatus.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.BlueViolet
+        Me.ClientSize = New System.Drawing.Size(446, 409)
+        Me.Controls.Add(Me.lblstatus)
+        Me.Controls.Add(Me.lbltalent)
+        Me.Controls.Add(Me.lblage)
+        Me.Controls.Add(Me.lblname)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblname As Label
+    Friend WithEvents lblage As Label
+    Friend WithEvents lbltalent As Label
+    Friend WithEvents lblstatus As Label
+End Class
---
+++ Practice_Sample/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub btn_Click(sender As Object, e As EventArgs) Handles btn.Click
+        lblname.Text = "Ken Vismanos"
+        lblage.Text = "22"
+        lblstatus.Text = "Sing/Dance/Tanan"
+        lbltalent.Text = "Busog"
+
+    End Sub
+
+End Class
